265d778e57cbe922268ab34705cceddc526f5cb1
lashaiakobadze <70689627+lashaiakobadze@users.noreply.github.com>
2020-11-05 20:28:11 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
 
 // require and use "multer"...
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.get("/hello", function(req, res) {
+  res.json({ greetings: "Hello, API" });
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Node.js listening ...");
+});
+
+let multer = require("multer");
+app.post("/api/fileanalyse", multer().single("upfile"), (req, res) => {
+  let respObj = {};
+  respObj["name"] = req.file.originalname;
+  respObj["type"] = req.file.mimetype;
+  respObj["size"] = req.file.size;
+  res.json(respObj);
 });
